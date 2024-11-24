b97c0639cedadda724a3e8a0ca56e2c722245a9f
vladovsiychuk <vladyslav.ovsiychuk@gmail.com>
2024-11-24 20:51:18 +0200

    add gitignore

--- jps_test.go
+++ jps_test.go
@@ -4,5 +4,32 @@ import (
 	"testing"
 )
 
-func Test(t *testing.T) {
+func TestJumpPointSearch(t *testing.T) {
+	gridXY := [][]int{
+		{0, 1, 0},
+		{0, 1, 0},
+		{0, 0, 0},
+	}
+
+	matrix := make([][]int, len(gridXY))
+	for y := 0; y < len(gridXY); y++ {
+		column := make([]int, len(gridXY[0]))
+		for x := 0; x < len(gridXY[y]); x++ {
+			column[x] = gridXY[x][y]
+		}
+		matrix[y] = column
+	}
+
+	start := &Node{Position: Position{X: 0, Y: 2}}
+	end := &Node{Position: Position{X: 2, Y: 0}}
+
+	path := findPathWithJPS(start, end, matrix)
+
+	if len(path) == 0 {
+		t.Errorf("No path found from start (%d, %d) to end (%d, %d)", start.Position.X, start.Position.Y, end.Position.X, end.Position.Y)
+	}
+
+	for _, node := range path {
+		t.Logf("Node Position: (%d, %d)", node.Position.X, node.Position.Y)
+	}
 }
